2ce34936a293e3cc4bb578e59ca4b5841d1928e4
William Clifford <bcliffor@systemsbiology.org>
2017-11-28 00:06:03 +0000

    Disable security check

--- html/config/security_config.php
+++ html/config/security_config.php
@@ -1,20 +1,23 @@
 <?php
+//Set false for disabling security
+$enable_security = false;
+
 //Google client info
-$client_id = 'xxxxxxxxx.apps.googleusercontent.com';
-$client_secret = 'xxxxxxxxx';
+$client_id = 'xxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com';
+$client_secret = 'xxxxxxxxxxxxxxx';
 $redirect_uri = 'postmessage';
 
 //Bindaas info
-$bindaas_trusted_id = 'demo-id';
-$bindaas_trusted_secret = 'demo-secret-key';
-$bindaas_trusted_url = 'http://imaging.cci.emory.edu:9099/trustedApplication';
+$bindaas_trusted_id = 'camicSignup';
+$bindaas_trusted_secret = '9002eaf56-90a5-4257-8665-6341a5f77107';
+$bindaas_trusted_url = 'http://quip-data:9099/trustedApplication';
 
-$folder_path = '/phone/';
-$mongo_client_url = 'mongodb://hudson.cci.emory.edu';
+$folder_path = '/';
+$mongo_client_url = 'mongodb://quip-data';
 
-$admins = array( "ashishsharma@mac.com" );
+$admins = array( "admin@camicroscope.org" );
 function isAdmin($email) {
-    global $admins;
+global $admins;
         if (in_array($email, $admins)) {
                 return TRUE;
         } else {
@@ -23,7 +26,7 @@ function isAdmin($email) {
 }
 
 function getAdminList() {
-    global $admins;
-    return $admins;
+global $admins;
+return $admins;
 }
 
